2c3c8ac2fa59d296b7dd09354c1dd117b1f21ff1
KongYiFei <61506871+while-TuRe@users.noreply.github.com>
2021-07-12 18:46:42 +0800

    爬取网站图片资源

---
+++ downloadSrc.py
@@ -0,0 +1,14 @@
+
+import os
+from urllib.request import urlretrieve
+sides = ['axl','sag','corp1']
+def download():
+    for side in sides:
+        if not os.path.exists('./pictures/'+side):
+            os.mkdir('./pictures/'+side)
+        for i in range(0,127):#[0..126]
+            IMAGE_URL = 'http://www.med.harvard.edu/AANLIB/cases/caseNA/gr/'+side+'/'+str(i).zfill(3)+'.png'
+            print(IMAGE_URL)
+            urlretrieve(IMAGE_URL, './pictures/'+side+'/'+str(i).zfill(3)+'.png')     
+if __name__=='__main__':
+    download()
